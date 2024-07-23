2bc1adc09bb34ce19f1303078b1a50e765dee2ba
Chase Westlye <CWestlye@gmail.com>
2024-07-23 10:45:28 -0700

    Added queue_workers_if_queue_empty
    Also, github action

--- start.py
+++ start.py
@@ -1,2 +1,4 @@
-from tasks import send_queue_workers_if_queue_empty
-send_queue_workers_if_queue_empty('farts')
+from tasks import queue_workers_if_queue_empty
+
+print('start.py executed!')
+queue_workers_if_queue_empty('farts')
--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
-from celery import Celery
-from celery.schedules import crontab
-import logging, os, time
+from celery import Celery 
+import logging, os, time, requests
 
 # create logger
 logger = logging.getLogger('boilest_logs')
@@ -31,20 +30,81 @@ def celery_url_path(thing):
     logger.debug('celery_url_path is: ' + thing)
     return thing
 
+
 app = Celery('worker_queue', broker = celery_url_path('amqp://') )
+app.conf.task_default_queue = 'worker_queue'
+app.conf.task_queues = {
+    'worker_queue': {
+        'exchange': 'tasks',
+        'exchange_type': 'direct',
+        'routing_key': 'worker_queue',
+        'queue_arguments': {'x-max-priority': 10},
+    }
+}
+app.conf.task_routes = {
+    'locate_files': {'queue': 'worker_queue'},
+    'requires_encoding': {'queue': 'worker_queue'},
+    'process_ffmpeg': {'queue': 'worker_queue'}
+}
+
+
+def queue_workers_if_queue_empty(arg):
+    logger.debug(arg)
+    try:
+        queue_depth = check_queue('worker_queue')        
+        logger.debug(f'Current Worker queue depth is: {queue_depth}')       
+        if queue_depth == 0:
+            logger.info('Starting locate_files')
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+            send_task()
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+            # >>>>>>>>>>><<<<<<<<<<<<<<<<
+        elif queue_depth > 0:
+            logger.debug(f'{queue_depth} tasks in queue. No rescan needed at this time.')
+        else:
+            logger.error('Something went wrong checking the Worker Queue')
+    
+    except Exception as e:
+        logger.error(f"Error in queue_workers_if_queue_empty: {e}")
 
 
 def send_task():
-    task_name = 'tasks.queue_workers_if_queue_empty'
+    task_name = 'tasks.locate_files'
     task_arg = 'farts'  # Replace 'your_argument' with the actual argument you want to pass
-    queue_name = 'boilest_worker'
+    queue_name = 'worker_queue'
     
-    app.send_task(task_name, args=[task_arg], queue=queue_name)
+    app.send_task(task_name, args=[task_arg], queue=queue_name, priority=1)
     logger.info(f"Task sent to queue: {queue_name}")
+
+
+def check_queue(queue_name):
+    try:
+        rabbitmq_host = 'http://' + os.environ.get('rabbitmq_host')
+        rabbitmq_port = os.environ.get('rabbitmq_port')
+        user = os.environ.get('celery_user')
+        password = os.environ.get('celery_password')
+        celery_vhost = os.environ.get('celery_vhost')
+
+        url = f"{rabbitmq_host}:{rabbitmq_port}/api/queues/{celery_vhost}/{queue_name}"
+        logger.debug(f'Checking RabbitMQ queue depth for: {queue_name}')
+
+        response = requests.get(url, auth=(user, password))
+        response.raise_for_status()  # Ensure we raise an exception for HTTP errors
+
+        worker_queue = response.json()
+        queue_depth = worker_queue.get("messages_unacknowledged", 0)
+
+        logger.debug (f'check_queue queue depth is: ' + str(queue_depth))
+        return queue_depth
+    except Exception as e:
+        logger.error(f"Error getting active tasks count: {e}")
+        return -1 # Return -1 to indicate an error
+
 
 if __name__ == "__main__":
     while True:
-        send_task()
+        queue_workers_if_queue_empty('farts_schedule')
         # Wait for one hour (3600 seconds)
         time.sleep(3600)
 
